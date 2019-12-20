a8dd8ade325ae535422d1d65e9a3e379815b758b
Alexander Wilcots <alexander.wilcots@endurance.com>
2019-12-20 07:57:13 -0600

    5.1 - wip - moving to local machine, more power

--- main.go
+++ main.go
@@ -2,52 +2,128 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"os"
 )
 
-func add(a, b int) int {
-	return a + b
+type opFunc func([]int, *[]int)
+
+func getOpFunc(opc int) (opFunc, int) {
+	switch opc {
+	case 1, 11101, 11001, 10001, 1001, 101:
+		return opc1, 4
+	case 2, 11102, 11002, 10002, 1002, 102:
+		return opc2, 4
+	case 3:
+		return opc3, 2
+	case 4:
+		return opc4, 2
+	case 99:
+		return opc99, 1
+	}
+
+	return nil, 0
 }
 
-func multiply(a, b int) int {
-	return a * b
+func opcGeneric(instructions []int, fullCode *[]int) (int, int, int) {
+	pModes := getPModes(instructions[0])
+
+	var (
+		operandA    int
+		operandB    int
+		destination int
+	)
+
+	if pModes[2] == 1 {
+		operandA = instructions[1] //Immediate Mode
+	} else {
+		operandA = (*fullCode)[instructions[1]] //Position Mode
+	}
+
+	if pModes[1] == 1 {
+		operandB = instructions[2]
+	} else {
+		operandB = (*fullCode)[instructions[2]]
+	}
+
+	destination = instructions[3]
+
+	return operandA, operandB, destination
+}
+
+func opc1(instructions []int, fullCode *[]int) {
+	p1, p2, dest := opcGeneric(instructions, fullCode)
+
+	result := p1 + p2
+	(*fullCode)[dest] = result
+}
+
+func opc2(instructions []int, fullCode *[]int) {
+	p1, p2, dest := opcGeneric(instructions, fullCode)
+
+	result := p1 * p2
+	(*fullCode)[dest] = result
+}
+
+func opc3(instructions []int, fullCode *[]int) {
+	var input int
+	fmt.Scanf("Please enter device id: %d", &input)
+
+	(*fullCode)[instructions[1]] = input
+}
+
+func opc4(instructions []int, fullCode *[]int) {
+	fmt.Printf("Output: %d\n",
+		(*fullCode)[instructions[1]],
+	)
+}
+
+func opc99(instructions []int, fullCode *[]int) {
+	fmt.Println((*fullCode)[0])
+	os.Exit(0)
+}
+
+func getPModes(op int) []int {
+	var units []int
+	opc := float64(op)
+
+	for i := 5; i >= 0; i-- {
+		pow10 := math.Pow10(i)
+
+		unit := opc / pow10
+		unit = math.Floor(unit)
+
+		opc -= (pow10 * unit)
+
+		units = append(units, int(unit))
+	}
+
+	return units[:3]
 }
 
 func main() {
-	intCode := []int{1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,6,19,1,9,19,23,1,6,23,27,1,10,27,31,1,5,31,35,2,6,35,39,1,5,39,43,1,5,43,47,2,47,6,51,1,51,5,55,1,13,55,59,2,9,59,63,1,5,63,67,2,67,9,71,1,5,71,75,2,10,75,79,1,6,79,83,1,13,83,87,1,10,87,91,1,91,5,95,2,95,10,99,2,9,99,103,1,103,6,107,1,107,10,111,2,111,10,115,1,115,6,119,2,119,9,123,1,123,6,127,2,127,10,131,1,131,6,135,2,6,135,139,1,139,5,143,1,9,143,147,1,13,147,151,1,2,151,155,1,10,155,0,99,2,14,0,0}
+	intCode := []int{3, 225, 1, 225, 6, 6, 1100, 1, 238, 225, 104, 0, 1002, 36, 25, 224, 1001, 224, -2100, 224, 4, 224, 1002, 223, 8, 223, 101, 1, 224, 224, 1, 223, 224, 223, 1102, 31, 84, 225, 1102, 29, 77, 225, 1, 176, 188, 224, 101, -42, 224, 224, 4, 224, 102, 8, 223, 223, 101, 3, 224, 224, 1, 223, 224, 223, 2, 196, 183, 224, 1001, 224, -990, 224, 4, 224, 1002, 223, 8, 223, 101, 7, 224, 224, 1, 224, 223, 223, 102, 14, 40, 224, 101, -1078, 224, 224, 4, 224, 1002, 223, 8, 223, 1001, 224, 2, 224, 1, 224, 223, 223, 1001, 180, 64, 224, 101, -128, 224, 224, 4, 224, 102, 8, 223, 223, 101, 3, 224, 224, 1, 223, 224, 223, 1102, 24, 17, 224, 1001, 224, -408, 224, 4, 224, 1002, 223, 8, 223, 101, 2, 224, 224, 1, 223, 224, 223, 1101, 9, 66, 224, 1001, 224, -75, 224, 4, 224, 1002, 223, 8, 223, 1001, 224, 6, 224, 1, 223, 224, 223, 1102, 18, 33, 225, 1101, 57, 64, 225, 1102, 45, 11, 225, 1101, 45, 9, 225, 1101, 11, 34, 225, 1102, 59, 22, 225, 101, 89, 191, 224, 1001, 224, -100, 224, 4, 224, 1002, 223, 8, 223, 1001, 224, 1, 224, 1, 223, 224, 223, 4, 223, 99, 0, 0, 0, 677, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1105, 0, 99999, 1105, 227, 247, 1105, 1, 99999, 1005, 227, 99999, 1005, 0, 256, 1105, 1, 99999, 1106, 227, 99999, 1106, 0, 265, 1105, 1, 99999, 1006, 0, 99999, 1006, 227, 274, 1105, 1, 99999, 1105, 1, 280, 1105, 1, 99999, 1, 225, 225, 225, 1101, 294, 0, 0, 105, 1, 0, 1105, 1, 99999, 1106, 0, 300, 1105, 1, 99999, 1, 225, 225, 225, 1101, 314, 0, 0, 106, 0, 0, 1105, 1, 99999, 8, 226, 677, 224, 1002, 223, 2, 223, 1006, 224, 329, 1001, 223, 1, 223, 108, 226, 226, 224, 1002, 223, 2, 223, 1006, 224, 344, 1001, 223, 1, 223, 7, 677, 226, 224, 102, 2, 223, 223, 1005, 224, 359, 101, 1, 223, 223, 7, 226, 677, 224, 102, 2, 223, 223, 1006, 224, 374, 101, 1, 223, 223, 1008, 677, 226, 224, 1002, 223, 2, 223, 1006, 224, 389, 101, 1, 223, 223, 8, 677, 677, 224, 1002, 223, 2, 223, 1005, 224, 404, 101, 1, 223, 223, 8, 677, 226, 224, 102, 2, 223, 223, 1005, 224, 419, 1001, 223, 1, 223, 1107, 677, 226, 224, 102, 2, 223, 223, 1005, 224, 434, 1001, 223, 1, 223, 1107, 226, 677, 224, 1002, 223, 2, 223, 1006, 224, 449, 1001, 223, 1, 223, 107, 677, 226, 224, 1002, 223, 2, 223, 1005, 224, 464, 1001, 223, 1, 223, 1008, 677, 677, 224, 1002, 223, 2, 223, 1006, 224, 479, 1001, 223, 1, 223, 1108, 677, 226, 224, 1002, 223, 2, 223, 1006, 224, 494, 1001, 223, 1, 223, 1108, 677, 677, 224, 1002, 223, 2, 223, 1006, 224, 509, 1001, 223, 1, 223, 107, 677, 677, 224, 1002, 223, 2, 223, 1005, 224, 524, 101, 1, 223, 223, 1007, 677, 226, 224, 102, 2, 223, 223, 1005, 224, 539, 1001, 223, 1, 223, 1107, 226, 226, 224, 1002, 223, 2, 223, 1006, 224, 554, 1001, 223, 1, 223, 1008, 226, 226, 224, 1002, 223, 2, 223, 1006, 224, 569, 101, 1, 223, 223, 1108, 226, 677, 224, 1002, 223, 2, 223, 1006, 224, 584, 101, 1, 223, 223, 108, 677, 677, 224, 1002, 223, 2, 223, 1006, 224, 599, 1001, 223, 1, 223, 1007, 677, 677, 224, 102, 2, 223, 223, 1006, 224, 614, 101, 1, 223, 223, 107, 226, 226, 224, 102, 2, 223, 223, 1006, 224, 629, 101, 1, 223, 223, 1007, 226, 226, 224, 102, 2, 223, 223, 1005, 224, 644, 1001, 223, 1, 223, 108, 226, 677, 224, 102, 2, 223, 223, 1005, 224, 659, 1001, 223, 1, 223, 7, 677, 677, 224, 102, 2, 223, 223, 1006, 224, 674, 1001, 223, 1, 223, 4, 223, 99, 226}
 
-	startIdx := 0
-	endIdx 	 := 3
-
-	var result int
+	var (
+		curCode  []int
+		startIdx int
+		endIdx   int
+	)
 
 	for {
-		if intCode[ startIdx ] == 99 {
-			result = intCode[0]
-			break
-		}
+		//Get process for handling opcode and amount of required
+		//parameters
+		process, paramCount := getOpFunc(intCode[startIdx])
+		endIdx += paramCount
 
-		if intCode[ startIdx ] != 1 && intCode[ startIdx ] != 2 {
-			result = intCode[0]
-			fmt.Print("Errored: ")
-			break
-		}
+		//Load variable with current instruction snippit
+		curCode = intCode[startIdx:endIdx]
 
-		curCode := intCode[ startIdx : endIdx +1 ]
-		var num int
+		//Use returned function to process code
+		process(curCode, &intCode)
 
-		switch curCode[0] {
-		case 1:
-			num = add( intCode[ curCode[1] ], intCode[ curCode[2] ])
-		case 2:
-			num = multiply( intCode[ curCode[1] ], intCode[ curCode[2] ])
-		}
-		
-		intCode[ curCode[3] ] = num
-
-		startIdx += 4
-		endIdx   += 4
+		//Move forward the index by the number of consumed
+		//parameters
+		startIdx += paramCount
 	}
-
-	fmt.Println(result)
-
 }
